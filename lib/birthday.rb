6b62ebab7b5fbdc911b46609fa0f7eeeda066dd1
Zecharias Mengis <zmengis@gmail.com>
2021-02-16 16:03:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,5 @@
 # }
 
 def happy_birthday(birthday_kids)
-  def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
+  
 end
